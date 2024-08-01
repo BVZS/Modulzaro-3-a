fa66a14bb0acb03f4d3b9941452ac35e221c7d19
Balazs Fajd <mrfajd@gmail.com>
2024-08-01 22:07:54 +0200

    void lottoMegjelenit, void totoMegjelenit

--- src/main/MindentTudoGep.java
+++ src/main/MindentTudoGep.java
@@ -6,12 +6,13 @@ import java.util.Random;
 
 public class MindentTudoGep {
     private int[] lottoSzamok = new int[5];
-    private String[] totoEredmenyek;
+    private char[] totoEredmenyek = new char[14];
     private String[][] zenek;
     
     Random rnd = new Random();
     
     public MindentTudoGep() {
+        // lottó
         for (int i = 0; i < lottoSzamok.length; i++) {
             while (true) {
                 int number = rnd.nextInt(90) + 1;
@@ -21,6 +22,24 @@ public class MindentTudoGep {
                 }
             }
         }
+        
+        // totó
+        for (int i = 0; i < totoEredmenyek.length; i++) {
+            char eredmeny = '1';
+            switch (rnd.nextInt(3)) {
+                case 0:
+                    eredmeny = '1';
+                    break;
+                case 1:
+                    eredmeny = '2';
+                    break;
+                case 2:
+                    eredmeny = 'x';
+                    break;
+            }
+            totoEredmenyek[i] = eredmeny;
+        }
+        
     }
     
     private static boolean tartalmazza(int[] array, int value) {
@@ -41,5 +60,26 @@ public class MindentTudoGep {
         }
         
         return str.substring(0, str.length() - 2);
+    }
+    
+    public String totoMegjelenit() {
+        String str = "mérkőzések: 1 2 3 4 5 6 7 8 9 10 11 12 13 13+1\n";
+        str += "eredmények: %c %c %c %c %c %c %c %c %c %2c %2c %2c %2c %4c".formatted(
+            this.totoEredmenyek[0],
+            this.totoEredmenyek[1],
+            this.totoEredmenyek[2],
+            this.totoEredmenyek[3],
+            this.totoEredmenyek[4],
+            this.totoEredmenyek[5],
+            this.totoEredmenyek[6],
+            this.totoEredmenyek[7],
+            this.totoEredmenyek[8],
+            this.totoEredmenyek[9],
+            this.totoEredmenyek[10],
+            this.totoEredmenyek[11],
+            this.totoEredmenyek[12],
+            this.totoEredmenyek[13]
+        );
+        return str;
     }
 }
